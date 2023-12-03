62175fb75fdff2a3b7d80d014713686a1e121af8
Mohamed-Abdelrazeq <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2023-12-03 20:21:43 +0200

    reformat images add bg-image

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'service-1': "url('./assets/services/1.jpg')",
+        'service-2': "url('./assets/services/2.jpg')",
+        'service-3': "url('./assets/services/3.jpg')",
+        'service-4': "url('./assets/services/4.jpg')",
+        'service-5': "url('./assets/services/5.jpg')",
+      }
+    },
   },
   plugins: [],
 }
